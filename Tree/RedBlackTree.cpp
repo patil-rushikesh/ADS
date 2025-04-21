e6efc381deea3a417ac3cb3aee082a0ffae0e606
Rushikesh Patil <prushikeh@gmail.com>
2025-04-21 20:27:24 +0530

    renamed file

--- Tree/RedBlackTree.cpp
+++ Tree/RedBlackTree.cpp
@@ -1,27 +1,31 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-struct treenode {
+struct treenode
+{
     int data;
-    treenode* left;
-    treenode* right;
+    treenode *left;
+    treenode *right;
     bool color; // true for Red, false for Black
-    treenode* parent;
+    treenode *parent;
 
-    treenode(int val) {
+    treenode(int val)
+    {
         data = val;
         left = right = parent = NULL;
         color = true; // New nodes are always red initially
     }
 };
 
-class RedBlackTree {
+class RedBlackTree
+{
 private:
-    treenode* root;
+    treenode *root;
 
-    void rotateLeft(treenode* node) {
-        treenode* rightChild = node->right;
+    void rotateLeft(treenode *node)
+    {
+        treenode *rightChild = node->right;
         node->right = rightChild->left;
         if (rightChild->left != NULL)
             rightChild->left->parent = node;
@@ -36,8 +40,9 @@ private:
         node->parent = rightChild;
     }
 
-    void rotateRight(treenode* node) {
-        treenode* leftChild = node->left;
+    void rotateRight(treenode *node)
+    {
+        treenode *leftChild = node->left;
         node->left = leftChild->right;
         if (leftChild->right != NULL)
             leftChild->right->parent = node;
@@ -52,71 +57,81 @@ private:
         node->parent = leftChild;
     }
 
-void fixInsert(treenode* node) {
-    while (node != root && node->parent->color == true) {
-        treenode* parent = node->parent;
-        treenode* grandparent = parent->parent;
+    void fixInsert(treenode *node)
+    {
+        while (node != root && node->parent->color == true)
+        {
+            treenode *parent = node->parent;
+            treenode *grandparent = parent->parent;
 
-        // Case A: Parent is left child of grandparent
-        if (parent == grandparent->left) {
-            treenode* uncle = grandparent->right;
+            // Case A: Parent is left child of grandparent
+            if (parent == grandparent->left)
+            {
+                treenode *uncle = grandparent->right;
 
-            // Case 1: Uncle is red -> just recolor
-            if (uncle != NULL && uncle->color == true) {
-                parent->color = false;
-                uncle->color = false;
-                grandparent->color = true;
-                node = grandparent; // Move up to fix violations up the tree
-            }
-            else {
-                // Case 2: Node is right child -> rotate left on parent
-                if (node == parent->right) {
-                    node = parent;
-                    rotateLeft(node);
-                    parent = node->parent;
-                    grandparent = parent->parent;
+                // Case 1: Uncle is red -> just recolor
+                if (uncle != NULL && uncle->color == true)
+                {
+                    parent->color = false;
+                    uncle->color = false;
+                    grandparent->color = true;
+                    node = grandparent; // Move up to fix violations up the tree
                 }
+                else
+                {
+                    // Case 2: Node is right child -> rotate left on parent
+                    if (node == parent->right)
+                    {
+                        node = parent;
+                        rotateLeft(node);
+                        parent = node->parent;
+                        grandparent = parent->parent;
+                    }
 
-                // Case 3: Node is left child -> rotate right on grandparent
-                parent->color = false;
-                grandparent->color = true;
-                rotateRight(grandparent);
+                    // Case 3: Node is left child -> rotate right on grandparent
+                    parent->color = false;
+                    grandparent->color = true;
+                    rotateRight(grandparent);
+                }
+            }
+
+            // Case B: Parent is right child of grandparent
+            else
+            {
+                treenode *uncle = grandparent->left;
+
+                // Case 1: Uncle is red -> just recolor
+                if (uncle != NULL && uncle->color == true)
+                {
+                    parent->color = false;
+                    uncle->color = false;
+                    grandparent->color = true;
+                    node = grandparent;
+                }
+                else
+                {
+                    // Case 2: Node is left child -> rotate right on parent
+                    if (node == parent->left)
+                    {
+                        node = parent;
+                        rotateRight(node);
+                        parent = node->parent;
+                        grandparent = parent->parent;
+                    }
+
+                    // Case 3: Node is right child -> rotate left on grandparent
+                    parent->color = false;
+                    grandparent->color = true;
+                    rotateLeft(grandparent);
+                }
             }
         }
 
-        // Case B: Parent is right child of grandparent
-        else {
-            treenode* uncle = grandparent->left;
-
-            // Case 1: Uncle is red -> just recolor
-            if (uncle != NULL && uncle->color == true) {
-                parent->color = false;
-                uncle->color = false;
-                grandparent->color = true;
-                node = grandparent;
-            }
-            else {
-                // Case 2: Node is left child -> rotate right on parent
-                if (node == parent->left) {
-                    node = parent;
-                    rotateRight(node);
-                    parent = node->parent;
-                    grandparent = parent->parent;
-                }
-
-                // Case 3: Node is right child -> rotate left on grandparent
-                parent->color = false;
-                grandparent->color = true;
-                rotateLeft(grandparent);
-            }
-        }
+        root->color = false; // Root must always be black
     }
 
-    root->color = false; // Root must always be black
-}
-
-
-    void transplant(treenode* u, treenode* v) {
+    void transplant(treenode *u, treenode *v)
+    {
         if (u->parent == NULL)
             root = v;
         else if (u == u->parent->left)
@@ -127,28 +142,37 @@ void fixInsert(treenode* node) {
             v->parent = u->parent;
     }
 
-    treenode* minimum(treenode* node) {
+    treenode *minimum(treenode *node)
+    {
         while (node->left != NULL)
             node = node->left;
         return node;
     }
 
-    void fixDelete(treenode* x) {
-        while (x != root && (x == NULL || x->color == false)) {
-            if (x == x->parent->left) {
-                treenode* sibling = x->parent->right;
-                if (sibling->color == true) {
+    void fixDelete(treenode *x)
+    {
+        while (x != root && (x == NULL || x->color == false))
+        {
+            if (x == x->parent->left)
+            {
+                treenode *sibling = x->parent->right;
+                if (sibling->color == true)
+                {
                     sibling->color = false;
                     x->parent->color = true;
                     rotateLeft(x->parent);
                     sibling = x->parent->right;
                 }
                 if ((sibling->left == NULL || sibling->left->color == false) &&
-                    (sibling->right == NULL || sibling->right->color == false)) {
+                    (sibling->right == NULL || sibling->right->color == false))
+                {
                     sibling->color = true;
                     x = x->parent;
-                } else {
-                    if (sibling->right == NULL || sibling->right->color == false) {
+                }
+                else
+                {
+                    if (sibling->right == NULL || sibling->right->color == false)
+                    {
                         if (sibling->left != NULL)
                             sibling->left->color = false;
                         sibling->color = true;
@@ -162,20 +186,27 @@ void fixInsert(treenode* node) {
                     rotateLeft(x->parent);
                     x = root;
                 }
-            } else {
-                treenode* sibling = x->parent->left;
-                if (sibling->color == true) {
+            }
+            else
+            {
+                treenode *sibling = x->parent->left;
+                if (sibling->color == true)
+                {
                     sibling->color = false;
                     x->parent->color = true;
                     rotateRight(x->parent);
                     sibling = x->parent->left;
                 }
                 if ((sibling->right == NULL || sibling->right->color == false) &&
-                    (sibling->left == NULL || sibling->left->color == false)) {
+                    (sibling->left == NULL || sibling->left->color == false))
+                {
                     sibling->color = true;
                     x = x->parent;
-                } else {
-                    if (sibling->left == NULL || sibling->left->color == false) {
+                }
+                else
+                {
+                    if (sibling->left == NULL || sibling->left->color == false)
+                    {
                         if (sibling->right != NULL)
                             sibling->right->color = false;
                         sibling->color = true;
@@ -195,12 +226,15 @@ void fixInsert(treenode* node) {
             x->color = false;
     }
 
-    void deleteNode(treenode* node, int key) {
-        treenode* z = NULL;
-        treenode* x, *y;
-        
-        while (node != NULL) {
-            if (node->data == key) {
+    void deleteNode(treenode *node, int key)
+    {
+        treenode *z = NULL;
+        treenode *x, *y;
+
+        while (node != NULL)
+        {
+            if (node->data == key)
+            {
                 z = node;
                 break;
             }
@@ -210,27 +244,36 @@ void fixInsert(treenode* node) {
                 node = node->right;
         }
 
-        if (z == NULL) {
+        if (z == NULL)
+        {
             cout << "Key not found in the tree\n";
             return;
         }
 
         y = z;
         bool y_original_color = y->color;
-        if (z->left == NULL) {
+        if (z->left == NULL)
+        {
             x = z->right;
             transplant(z, z->right);
-        } else if (z->right == NULL) {
+        }
+        else if (z->right == NULL)
+        {
             x = z->left;
             transplant(z, z->left);
-        } else {
+        }
+        else
+        {
             y = minimum(z->right);
             y_original_color = y->color;
             x = y->right;
-            if (y->parent == z) {
+            if (y->parent == z)
+            {
                 if (x != NULL)
                     x->parent = y;
-            } else {
+            }
+            else
+            {
                 transplant(y, y->right);
                 y->right = z->right;
                 if (y->right != NULL)
@@ -250,16 +293,19 @@ void fixInsert(treenode* node) {
 public:
     RedBlackTree() { root = NULL; }
 
-    void insert(int data) {
-        treenode* node = new treenode(data);
-        if (root == NULL) {
+    void insert(int data)
+    {
+        treenode *node = new treenode(data);
+        if (root == NULL)
+        {
             root = node;
             root->color = false;
             return;
         }
-        treenode* temp = root;
-        treenode* parent = NULL;
-        while (temp != NULL) {
+        treenode *temp = root;
+        treenode *parent = NULL;
+        while (temp != NULL)
+        {
             parent = temp;
             if (node->data < temp->data)
                 temp = temp->left;
@@ -274,66 +320,77 @@ public:
         fixInsert(node);
     }
 
-    void insertMultiple() {
+    void insertMultiple()
+    {
         int count, value;
         cout << "How many values to insert? ";
         cin >> count;
         cout << "Enter " << count << " values separated by space: ";
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++)
+        {
             cin >> value;
             insert(value);
         }
     }
 
-    void remove(int data) {
+    void remove(int data)
+    {
         deleteNode(root, data);
     }
 
-    void levelOrderDisplay() {
-        if (root == NULL) {
+    void levelOrderDisplay()
+    {
+        if (root == NULL)
+        {
             cout << "Tree is empty!\n";
             return;
         }
-        queue<treenode*> q;
+        queue<treenode *> q;
         q.push(root);
-        while (!q.empty()) {
-            treenode* temp = q.front();
+        while (!q.empty())
+        {
+            treenode *temp = q.front();
             q.pop();
             cout << temp->data << (temp->color ? "(R) " : "(B) ");
-            if (temp->left != NULL) q.push(temp->left);
-            if (temp->right != NULL) q.push(temp->right);
+            if (temp->left != NULL)
+                q.push(temp->left);
+            if (temp->right != NULL)
+                q.push(temp->right);
         }
         cout << endl;
     }
 };
 
-int main() {
+int main()
+{
     RedBlackTree tree;
     int choice, data;
-    while (true) {
+    while (true)
+    {
         cout << "\n1. Insert\n2. Insert Multiple\n3. Level Order Display\n4. Delete\n5. Exit\nEnter choice: ";
         cin >> choice;
-        switch (choice) {
-            case 1:
-                cout << "Enter data: ";
-                cin >> data;
-                tree.insert(data);
-                break;
-            case 2:
-                tree.insertMultiple();
-                break;
-            case 3:
-                tree.levelOrderDisplay();
-                break;
-            case 4:
-                cout << "Enter data to delete: ";
-                cin >> data;
-                tree.remove(data);
-                break;
-            case 5:
-                return 0;
-            default:
-                cout << "Invalid choice!\n";
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter data: ";
+            cin >> data;
+            tree.insert(data);
+            break;
+        case 2:
+            tree.insertMultiple();
+            break;
+        case 3:
+            tree.levelOrderDisplay();
+            break;
+        case 4:
+            cout << "Enter data to delete: ";
+            cin >> data;
+            tree.remove(data);
+            break;
+        case 5:
+            return 0;
+        default:
+            cout << "Invalid choice!\n";
         }
     }
 }
